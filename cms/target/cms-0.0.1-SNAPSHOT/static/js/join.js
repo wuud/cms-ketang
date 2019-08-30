10b397a3e8881d0c26f87f8bdb8d16cf7f6a3583
wuud <wuu_dd@qq.com>
2019-08-30 21:09:24 +0800

    最终提交

--- cms/target/cms-0.0.1-SNAPSHOT/static/js/join.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $("#joinButton").on("click", function () {
-        $("#joinForm").submit();
-    });
-    $("#unJoinButton").on("click", function () {
-        $("#unJoinForm").submit();
-    });
-});
